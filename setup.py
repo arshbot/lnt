21829f871cb4ce6b7e3a76304b72d740e48801c2
Harsha Goli <harshagoli@gmail.com>
2019-05-05 18:41:34 -0400

    setup.py works with click

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="lnt",
-    version="0.0.1",
-    author="Harsha Goli",
-    author_email="harshagoli@gmail.com",
-    description="A better lncli",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/lnt",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+setup(
+    name='lnt',
+    version='0.1',
+    packages=['lnt', 'lnt.commands'],
+    include_package_data=True,
+    install_requires=[
+        'click',
     ],
+    entry_points='''
+        [console_scripts]
+        lnt=lnt.cli:cli
+    ''',
 )
