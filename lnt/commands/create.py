aff0f999975f2a08ecfc87f8a39bc2a89bddb468
Harsha Goli <harshagoli@gmail.com>
2019-05-15 18:14:35 -0400

    Imports, cleanups

--- lnt/commands/create.py
+++ lnt/commands/create.py
@@ -1,4 +1,14 @@
-import click
+# Mark standard lib imports
+from hashlib import sha1
+import os, codecs, argparse
+
+# Mark 3rd party lib imports
+import lnt.rpc.rpc_pb2 as ln, lnt.rpc.rpc_pb2_grpc as lnrpc
+from PyInquirer import style_from_dict, Token, prompt, Separator
+import click, grpc
+
+# Mark Local imports
+
 
 def create(ctx):
     click.echo("create called!")
@@ -9,7 +19,7 @@ def channel():
     return
 
 def rebalance():
-    click.echo("Rebalance created")
+
     return
 
 def invoice():
