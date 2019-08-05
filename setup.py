2b3b06e8f5b0942d4d40c5acb08bc0979d241aae
Harsha Goli <harshagoli@gmail.com>
2019-08-05 15:46:34 -0400

    Incremented version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lnt',
-    version='0.1',
+    version='0.2',
     packages=['lnt', 'lnt.commands'],
     include_package_data=True,
     install_requires=[
