6fdc5997ab29cb7ba1ac0b367267cf00cd3d7719
Harsha Goli <harshagoli@gmail.com>
2019-08-05 23:51:06 -0400

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup
 
 setup(
     name='lnt',
-    version='0.2',
-    packages=['lnt', 'lnt.commands'],
+    version='0.2.1',
+    packages=['lnt', 'lnt.commands', 'lnt.rpc', 'lnt.graphics', 'lnt.commands.utils'],
     include_package_data=True,
     install_requires=[
         'click',
+        'grpcio-tools',
+        'grpcio',
+        'PyInquirer',
     ],
     entry_points='''
         [console_scripts]
