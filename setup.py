9c6f344452323ff93b7a6a3763697d2ad81b4961
Harsha Goli <harshagoli@gmail.com>
2020-02-26 20:28:49 -0500

    v1.1.1
    Disabled lnt create as it's currently not implemented

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='lnt',
-    version='1.1.0',
+    version='1.1.1',
     author="arshbot",
     author_email="harshagoli@gmail.com",
     include_package_data=True,
